56face78b435dc9a3c1f50ca07c1cfa5f2fb061e
d14mittal <44315483+d14mittal@users.noreply.github.com>
2022-05-11 02:04:14 +0530

    Built Discovery Server for Microservice applications

---
+++ discovery-server-eureka/src/test/java/com/eureka/server/discoveryservereureka/DiscoveryServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.server.discoveryservereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
